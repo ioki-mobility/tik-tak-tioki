db276abc7ea5c9612b91f7cab8228e22fe7643e9
Christian Bäuerlein <fabrik42@gmail.com>
2022-06-06 14:11:40 +0200

    Adds ascii board state de/encode :D

---
+++ app/services/ascii_board_state.rb
@@ -0,0 +1,58 @@
+class AsciiBoardState
+  VALUES = %w[x o f]
+
+  TEMPLATE = ["|---+---+---|",
+              "| 0 | 1 | 2 |",
+              "|---+---+---|",
+              "| 3 | 4 | 5 |",
+              "|---+---+---|",
+              "| 6 | 7 | 8 |",
+              "|---+---+---|"]
+
+  def self.encode(board)
+    new.encode(board)
+  end
+
+  def self.decode(ascii_art)
+    new.decode(ascii_art)
+  end
+
+  def encode(board)
+    # {
+    #   "0" => "x",
+    #   "1" => "f",
+    #   "2" => "o",
+    #   ...
+    # }
+    sub_values = board.each_with_object({}).with_index {|(val, acc), index| acc[index.to_s] = val }
+
+    TEMPLATE.map { |line| line.gsub(/[0-8]/, sub_values) }
+  end
+
+  def decode(ascii_art)
+    lines = ascii_art.split("\n")
+    value_lines = [lines[1], lines[3], lines[5]]
+
+    value_lines.flat_map { |line| extract_line_values(line) }
+  end
+
+  def extract_line_values(line)
+    values = []
+    cell_value_found = true
+
+    line.chars.each do |char|
+      if char == '|' && cell_value_found == false
+        values << 'f'
+      elsif char == '|' && cell_value_found == true
+        cell_value_found = false
+      elsif char.downcase.in?(VALUES) && cell_value_found == false
+        cell_value_found = true
+        values << char
+      elsif char.downcase.in?(VALUES) && cell_value_found == true
+        raise 'Error, two values per cell found'
+      end
+    end
+
+    values
+  end
+end
---
+++ spec/services/ascii_board_state_spec.rb
@@ -0,0 +1,49 @@
+require "rails_helper"
+
+RSpec.describe AsciiBoardState do
+  describe '.encode' do
+    it 'can encode a board' do
+      result = described_class.encode(%w(x f o f o x o x f))
+
+      expected = ["|---+---+---|",
+                  "| x | f | o |",
+                  "|---+---+---|",
+                  "| f | o | x |",
+                  "|---+---+---|",
+                  "| o | x | f |",
+                  "|---+---+---|"]
+
+      expect(result).to eql(expected)
+    end
+  end
+
+  describe '.decode' do
+    it 'can decode a board' do
+      result = described_class.decode <<~BOARD
+        |---+---+---|
+        | x | f | o |
+        |---+---+---|
+        | f | o | x |
+        |---+---+---|
+        | o | x | f |
+        |---+---+---|
+      BOARD
+
+      expect(result).to eql(%w(x f o f o x o x f))
+    end
+
+    it 'can decode a board with blank cells' do
+      result = described_class.decode <<~BOARD
+        |---+---+---|
+        | x |   | o |
+        |---+---+---|
+        |   | o | x |
+        |---+---+---|
+        | o | x |   |
+        |---+---+---|
+      BOARD
+
+      expect(result).to eql(%w(x f o f o x o x f))
+    end
+  end
+end
